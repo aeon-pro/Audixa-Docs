42042454ced3da42b724b9351b48da57ea2e8e6e
Anurag Sharma <anurag@alactic.net>
2025-10-31 09:02:33 +0530

    Footer Fix

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -102,7 +102,7 @@ const config = {
             items: [
               {
                 label: 'Documentation',
-                to: '/intro',
+                to: '/',
               },
               {
                 label: 'API Reference',
